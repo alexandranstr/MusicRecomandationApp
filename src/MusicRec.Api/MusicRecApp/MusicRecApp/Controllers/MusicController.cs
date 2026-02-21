4e674412c5fa4a36d3905bde4bc12539b6063e86
alexandranstr <meda.nistor@student.unitbv.ro>
2026-02-21 20:46:11 +0200

    Swagger UI is working and test endpoint is functional

---
+++ src/MusicRec.Api/MusicRecApp/MusicRecApp/Controllers/MusicController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MusicRecApp.Model;
+
+namespace MusicRecApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MusicController : ControllerBase
+    {
+        [HttpGet("test-songs")]
+        public IActionResult GetSongs()
+        {
+            var songs = new List<Song>
+            {
+                new Song { Id = 1, Title = "Blinding Lights", Artist = "The Weeknd", Genre = "Pop", Tempo = 171, Energy = 0.8 },
+                new Song { Id = 2, Title = "Stairway to Heaven", Artist = "Led Zeppelin", Genre = "Rock", Tempo = 82, Energy = 0.5 }
+            };
+
+            return Ok(songs);
+        }
+    }
+}
---
+++ src/MusicRec.Api/MusicRecApp/MusicRecApp/Model/Song.cs
@@ -0,0 +1,12 @@
+﻿namespace MusicRecApp.Model
+{
+    public class Song
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Artist { get; set; } = string.Empty;
+        public string Genre { get; set; } = string.Empty;
+        public double Tempo { get; set; } 
+        public double Energy { get; set; } 
+    }
+}
